381b1b6891761983ea6f090a62721e5570ca6d54
yhb1834 <yhb1834@hanmail.net>
2021-07-15 01:41:46 +0900

    비밀번호 보호2

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.mongoURI //배포 시 HEROKU에서 가져오게 하기
+}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded 정보 쪼개기
@@ -10,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hanbyeol:yhk00927@boilerplate.cyfjz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
